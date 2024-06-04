168a91d353aa7a296704eb8c30550d862659e73f
Alexander Wolff <wolffaxn@gmail.com>
2024-06-04 21:10:10 +0200

    docker build with nix

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ] (system:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "arm64-linux" ] (system:
       let
         pkgs = (import nixpkgs) {
           inherit system;
